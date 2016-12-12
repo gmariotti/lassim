33bc33507bb042b6452363a53e8c80cc71f41a58
Guido Pio Mariotti <developer.mariotti@gmail.com>
2016-12-12 14:15:05 +0100

    .idea files as explained at [How-to-manage-projects-under-Version-Control-Systems](https://intellij-support.jetbrains.com/hc/en-us/articles/206544839-How-to-manage-projects-under-Version-Control-Systems)

---
+++ .idea/fileTemplates/Python Lassim.py
@@ -0,0 +1,7 @@
+#parse("user.py")
+#parse("version.py")
+
+__author__ = "${USER}"
+__copyright__ = "Copyright (C) 2016 ${USER}"
+__license__ = "GNU General Public License v3.0"
+__version__ = "${VERSION}"
---
+++ .idea/fileTemplates/includes/user.py
@@ -0,0 +1,1 @@
+#set( $USER = "Guido Pio Mariotti" )
