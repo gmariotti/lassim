0322af8845d3922c4459f68ac304a331e2a50034
Guido Pio Mariotti <developer.mariotti@gmail.com>
2017-02-03 16:49:52 +0100

    Configuration generalized
    Split in builder and parser

--- source/utilities/configuration.py
+++ source/utilities/configuration.py
@@ -1,8 +1,5 @@
 from configparser import ConfigParser
-from copy import deepcopy
-from typing import Iterable, Dict, List, Tuple
-
-from utilities.data_classes import InputFiles
+from typing import List, Tuple, NamedTuple, Callable, Dict
 
 __author__ = "Guido Pio Mariotti"
 __copyright__ = "Copyright (C) 2016 Guido Pio Mariotti"
@@ -10,104 +7,81 @@ __license__ = "GNU General Public License v3.0"
 __version__ = "0.3.0"
 
 
-class ConfigurationBuilder:
-    __data_section = "Input Data"
-    # FIXME - so stupid!
-    __data_dict = {
-        "network": False,
-        "data": False,
-        "times": False,
-        "perturbations": False
-    }
-
+class Configuration:
     def __init__(self, filename: str, allow_comments: bool = True):
-        self.__filename = filename
-        self.__parser = ConfigParser(allow_no_value=allow_comments)
-        self.__expected = dict()
+        self._filename = filename
+        self._parser = ConfigParser(allow_no_value=allow_comments)
 
-    def add_network(self, filename: str, *args: str) -> 'ConfigurationBuilder':
-        self.__check_section(self.__data_section)
-        self.__comment(args, self.__data_section)
-        self.__parser.set(self.__data_section, "network", filename)
+    def _check_section(self, section: str):
+        # FIXME - change to annotation
+        if not self._parser.has_section(section):
+            self._parser.add_section(section)
+
+    def _comment(self, comments: Tuple[str, ...], section: str):
+        for comment in comments:
+            if comment:
+                self._parser.set(section, "; " + comment)
+
+
+class ConfigurationBuilder(Configuration):
+    def __init__(self, filename: str, allow_comments: bool = True):
+        super(ConfigurationBuilder, self).__init__(filename, allow_comments)
+        self.__current_section = "DEFAULT"
+
+    def add_section(self, section: str, *args: str) -> 'ConfigurationBuilder':
+        self._check_section(section)
+        self._comment(args, section)
+        self.__current_section = section
         return self
 
-    def expect_network(self) -> 'ConfigurationBuilder':
-        self.__check_section(self.__data_section)
-        data_expected = self.__expect_section(
-            self.__data_section, self.__data_dict
-        )
-        data_expected["network"] = True
+    def add_key_value(self, key: str, value: str, *args: str
+                      ) -> 'ConfigurationBuilder':
+        self._comment(args, self.__current_section)
+        self._parser.set(self.__current_section, key, value)
         return self
 
-    def add_data(self, filenames: Iterable[str], *args: str
-                 ) -> 'ConfigurationBuilder':
-        self.__check_section(self.__data_section)
-        self.__comment(args, self.__data_section)
-        value = ", ".join(filenames)
-        self.__parser.set(self.__data_section, "data", value)
-        return self
-
-    def expect_data(self) -> 'ConfigurationBuilder':
-        self.__check_section(self.__data_section)
-        data_expected = self.__expect_section(
-            self.__data_section, self.__data_dict
-        )
-        data_expected["data"] = True
-        return self
-
-    def add_time(self, filename: str, *args: str) -> 'ConfigurationBuilder':
-        self.__check_section(self.__data_section)
-        self.__comment(args, self.__data_section)
-        self.__parser.set(self.__data_section, "times", filename)
-        return self
-
-    def expect_time(self) -> 'ConfigurationBuilder':
-        self.__check_section(self.__data_section)
-        data_expected = self.__expect_section(
-            self.__data_section, self.__data_dict
-        )
-        data_expected["times"] = True
+    def add_key_values(self, key: str, values: List[str], *args: str
+                       ) -> 'ConfigurationBuilder':
+        self._comment(args, self.__current_section)
+        value = ", ".join(values)
+        self._parser.set(self.__current_section, key, value)
         return self
 
     def build(self):
-        with open(self.__filename, "w") as config_file:
-            self.__parser.write(config_file)
+        with open(self._filename, "w") as config_file:
+            self._parser.write(config_file)
 
-    def parse(self) -> 'ConfigurationBuilder':
-        self.__parser.read(self.__filename)
-        expected_sections = self.__expected.keys()
-        if not set(expected_sections).issubset(self.__parser.sections()):
-            raise RuntimeError
+
+class ConfigurationParser(Configuration):
+    def __init__(self, filename: str, allow_comments: bool = True):
+        super(ConfigurationParser, self).__init__(filename, allow_comments)
+        self.__expected = {}
+
+    def define_section(self, section: str, *args: str) -> 'ConfigurationParser':
+        self.__expected.setdefault(section, [key for key in args])
         return self
 
-    def parse_input_data(self):
-        parser = self.parse()
-        expected_data = self.__expected[self.__data_section]
+    def parse_section(self, section: str, dataclass: NamedTuple,
+                      conversion: Callable[[Dict], Dict] = None) -> NamedTuple:
+        parsed_section = self.parse()._parser[section]
+        expected_keys = self.__expected[section]
         arguments = {}
-        for key, value in expected_data.items():
-            if value:
-                values = parser.__parser[self.__data_section][key].split(", ")
+        for key in expected_keys:
+            try:
+                values = parsed_section[key].split(", ")
                 if len(values) > 1:
                     arguments[key] = values
                 else:
                     arguments[key] = values[0]
-            else:
+            except KeyError:
                 arguments[key] = None
-        return InputFiles(**arguments)
+        if conversion is not None:
+            arguments = conversion(arguments)
+        return dataclass(**arguments)
 
-    def __check_section(self, section: str):
-        # FIXME - change to annotation
-        if not self.__parser.has_section(section):
-            self.__parser.add_section(section)
-
-    def __expect_section(self, section: str, expected: Dict) -> Dict:
-        # FIXME - change to annotation
-        section_data = self.__expected.setdefault(
-            section, deepcopy(expected)
-        )
-        return section_data
-
-    def __comment(self, comments: Tuple[str, ...], section: str):
-        for comment in comments:
-            if comment:
-                self.__parser.set(section, "; " + comment)
+    def parse(self) -> 'ConfigurationParser':
+        self._parser.read(self._filename)
+        expected_sections = self.__expected.keys()
+        if not set(expected_sections).issubset(self._parser.sections()):
+            raise RuntimeError
+        return self
